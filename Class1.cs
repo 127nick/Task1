8fbdcb487bfaefbb5c2cebb28bc17573ff1a715a
127nick <nikhil127nikam@gmail.com>
2023-05-17 17:59:38 +0530

    day 3

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class emp
+    {
+        public void show(int empno, out string name, out double basic)
+        {
+            if (empno==1)
+            {
+                name = "nikhil";
+                basic = 88995;
+
+            }else if (empno==2) 
+            {
+                name = "nikam";
+                basic = 99888;
+
+            }else
+            {
+                name = "not found";
+                basic = 0;
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int empno = 1;
+            string name;
+            double basic;
+        }
+
+    }
+}
--- array list.cs
+++ array list.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Demo1
+namespace Demo5
 {
     internal class Program
     {
