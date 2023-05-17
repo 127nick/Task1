7973545b8a5ef43372780f0b1504df204f3b28be
127nick <nikhil127nikam@gmail.com>
2023-05-17 17:05:12 +0530

    day2

---
+++ array list.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    internal class Program
+    {
+        public void show()
+        {
+            int[] a = new int[] { 10, 11, 12, 13, 15, 16 };
+            for (int i = 0; i <= a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+           Demo obj = new Demo();
+            obj.show();
+        }
+    }
+}
