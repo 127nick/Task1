6595205ae97b968c105c249fa7ced7669d80dc9e
127nick <nikhil127nikam@gmail.com>
2023-05-19 13:27:01 +0530

    19 may

---
+++ MYstaticlass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensionmethod
+{
+    static class MYstaticlass
+    {
+        public static void func3( this Program p)
+        {
+            Console.WriteLine("this is  3rd fuction ");
+        }
+    }
+}
---
+++ inheritance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inhertance_demo
+{ 
+            class visiemp : emp
+            {
+                public int vistingsal;
+                public int vistinghrs;
+            }
+            class peremp : emp
+            {
+                public int empsal;
+                public int emphrs;
+            }
+            class emp
+            {
+                public int empid;
+                public string name;
+                public int age;
+                public int contact_no;
+            }
+      
+        class program {
+            static void Main(string[] args)
+            {
+                peremp p= new peremp();   
+                p.empid = 1;
+                p.age = 2;  
+                p.contact_no = 3;
+                p.empsal = 4;
+                p.emphrs = 5;
+            
+
+
+          
+
+               Console.ReadLine
+                ();
+
+
+            }
+        }
+    }
+
